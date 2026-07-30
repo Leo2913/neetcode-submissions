41c9c04c6c2dba77e3cb2eeec0dc547f88bbf76c
Moss Liu <93119633+Leo2913@users.noreply.github.com>
2026-07-30 20:25:01 +0800

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int l = 1;
+        int r = 0;
+        for(int p : piles){
+            r = Math.max(p , r);
+        }
+        int res = r;
+        while(l <= r){
+            int k = (l + r) / 2;
+            long totalTime = 0;
+            for(int p : piles){
+                totalTime += Math.ceil((double) p / k);
+            }
+            if(totalTime <= h){
+                r = k - 1;
+                res = k;
+            }else{
+                l = k + 1;
+            }
+        }
+        return res;
+    }
+}
